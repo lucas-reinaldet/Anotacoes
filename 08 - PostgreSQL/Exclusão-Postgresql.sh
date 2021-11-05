83f62c40aae60664cd5e225c8ca613db2fc04a33
Lucas Reinaldet <lucas.reinaldet@pti.org.br>
2021-11-05 12:58:56 -0300

    Atualizaçao de anotaçoes

--- 08 - PostgreSQL/Exclusão-Postgresql.sh
+++ 08 - PostgreSQL/Exclusão-Postgresql.sh
@@ -2,7 +2,7 @@
  
 sudo apt-get purge postgresql-* pgadmin* -y
 
-sudo find /etc/ /var/lib/ -type d '(' -name "postgres*" -o -name "Postgresl*" -o -name "*pgadmin4*" ')' |  xargs --no-run-if-empty sudo rm -rf
+find /etc/ /var/lib/ -type d '(' -name "postgres*" -o -name "Postgresl*" -o -name "*pgadmin4*" ')' |  xargs --no-run-if-empty rm -rf
 
 sudo apt-get autoremove -y
 
