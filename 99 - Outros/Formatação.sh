f44ad2bcdd0688fc4d865db06607a9c675eb4aeb
Lucas <lucas.reinaldet@pti.org.br>
2022-01-29 17:05:30 -0300

    atl

--- 99 - Outros/Formatação.sh
+++ 99 - Outros/Formatação.sh
@@ -1,3 +1,16 @@
+function variaveis() {
+
+    # Variaveis utilizadas para configurar o Postgres
+    export DB_PWD_USER_POSTGRES='54321'
+    export DB_NEW_PORT_PG=9876
+    export DB_PORT_DEFAULT_PG=5432
+    export DB_USER_POSTGRES='postgres'
+    export DB_NAME_FILE_CONF_POSTGRESQL='postgresql.conf'
+    export DB_NAME_FILE_HBA_POSTGRESQL='pg_hba.conf'
+    export DB_FOLDER_PG_HBA=`find /etc/ /var/lib/ -ipath *13* -type f -name "${DB_NAME_FILE_HBA_POSTGRESQL}" 2>/dev/null`
+    export DB_FILE_CONF_PG=`find /etc/ /var/lib/ -ipath *13* -type f -name "${DB_NAME_FILE_CONF_POSTGRESQL}" 2>/dev/null`
+}
+
 function configuracoes_essenciais() {
     
     echo "Atualização e Upgrade do Sistema."
@@ -146,18 +159,7 @@ function alteracao_porta_padrao_postgresql() {
 }
 
 
-function variaveis() {
 
-    # Variaveis utilizadas para configurar o Postgres
-    export DB_PWD_USER_POSTGRES='30894'
-    export DB_NEW_PORT_PG=7979
-    export DB_PORT_DEFAULT_PG=5432
-    export DB_USER_POSTGRES='postgres'
-    export DB_NAME_FILE_CONF_POSTGRESQL='postgresql.conf'
-    export DB_NAME_FILE_HBA_POSTGRESQL='pg_hba.conf'
-    export DB_FOLDER_PG_HBA=`find /etc/ /var/lib/ -ipath *13* -type f -name "${DB_NAME_FILE_HBA_POSTGRESQL}" 2>/dev/null`
-    export DB_FILE_CONF_PG=`find /etc/ /var/lib/ -ipath *13* -type f -name "${DB_NAME_FILE_CONF_POSTGRESQL}" 2>/dev/null`
-}
 
 function unset_variaveis() {
 
@@ -180,15 +182,15 @@ function unset_variaveis() {
     reboot
 }
 
-function outros() {
+function manuais() {
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda-repo-ubuntu2004-11-6-local_11.6.0-510.39.01-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.0-510.39.01-1_amd64.deb
-sudo apt-key add /var/cuda-repo-ubuntu2004-11-6-local/7fa2af80.pub
-sudo apt-get update
-sudo apt-get -y install cuda
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+    sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+    wget https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda-repo-ubuntu2004-11-6-local_11.6.0-510.39.01-1_amd64.deb
+    sudo dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.0-510.39.01-1_amd64.deb
+    sudo apt-key add /var/cuda-repo-ubuntu2004-11-6-local/7fa2af80.pub
+    sudo apt-get update
+    sudo apt-get -y install cuda
 
     # GIT
 
